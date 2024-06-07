f4111e6ea1629850732cc7a4d35a8880d6ae748e
marciomaiabr <marcio.luis.maia@gmail.com>
2024-06-07 10:24:07 -0400

    adding a202406070945 project

---
+++ project/a202406070945/src/main/java/a202406070945/HelloWorld202406070954.java
@@ -0,0 +1,9 @@
+package a202406070945;
+
+public class HelloWorld202406070954 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld202406070954.main()");
+	}
+
+}
---
+++ project/a202406070945/src/test/java/a202406070945Tst/HelloWorld202406070954Test.java
@@ -0,0 +1,18 @@
+package a202406070945Tst;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import a202406070945.HelloWorld202406070954;
+
+class HelloWorld202406070954Test {
+
+	@Test
+	void test() {
+		System.out.println("HelloWorld202406070954Test.test()");
+		HelloWorld202406070954.main(null);
+		assertTrue(true);
+	}
+
+}
